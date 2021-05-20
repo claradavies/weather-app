ee8614f07e458f9daeab5f03bb42aa2fad81fa82
claradavies <clara.davies@gmail.com>
2021-05-20 11:18:42 +0100

    Install the new apiUrl for forecast

--- src/script.js
+++ src/script.js
@@ -46,7 +46,8 @@ function formatDate(timestamp) {
     return `${day}, ${month}, ${date} - ${hour}:${minute}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data);
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = "";
@@ -80,6 +81,12 @@ function displayForecast() {
     });
 
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "6ec0f1834392517657ca106147c93622";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -117,6 +124,8 @@ function showTemperature(response) {
     humidity.innerHTML = `${newHumidity}%`;
 
     celsius = response.data.main.temp;
+
+    getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -185,4 +194,3 @@ let fahrenheitLink = document.querySelector(".tempFahrenheit");
 fahrenheitLink.addEventListener("click", convertFahrenheit);
 
 searchCity("London");
-displayForecast();
